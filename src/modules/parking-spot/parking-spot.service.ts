5af5d768466f16d5d5d9627cb3763961c4597109
Fernando Soares, O Dev <fernandosoares.dev@gmail.com>
2023-01-24 22:21:34 -0300

    Response/Validations

--- src/modules/parking-spot/parking-spot.service.ts
+++ src/modules/parking-spot/parking-spot.service.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { CreateParkingSpotDto } from './dto/create-parking-spot.dto';
 import { UpdateParkingSpotDto } from './dto/update-parking-spot.dto';
@@ -8,6 +12,36 @@ export class ParkingSpotService {
   constructor(private prisma: PrismaService) {}
 
   async create(data: CreateParkingSpotDto) {
+    const { car_license_plate, spot_number, apartment, tower } = data;
+
+    const isPlateConflict = await this.existisByCarLicensePlate(
+      car_license_plate,
+    );
+    const isSpotConflict = await this.existisBySpotNumber(spot_number);
+    const isApartmentAndTowerConflict =
+      await this.existsApartmentAndTowerConflict(apartment, tower);
+
+    if (isPlateConflict) {
+      throw new ConflictException('LICENSE PLATE CONFLICT', {
+        cause: new Error(),
+        description: 'The provided license plate is already registered.',
+      });
+    }
+
+    if (isSpotConflict) {
+      throw new ConflictException('SPOT CONFLICT', {
+        cause: new Error(),
+        description: 'The provided parking spot is already in use.',
+      });
+    }
+
+    if (isApartmentAndTowerConflict) {
+      throw new ConflictException('APARTMENT/TOWER CONFLICT', {
+        cause: new Error(),
+        description: 'The provided apartment/tower is already in use.',
+      });
+    }
+
     return await this.prisma.spot.create({
       data,
     });
@@ -18,6 +52,7 @@ export class ParkingSpotService {
   }
 
   async findOne(id: string) {
+    await this.isValidResource(id);
     return await this.prisma.spot.findFirst({
       where: {
         id,
@@ -26,6 +61,8 @@ export class ParkingSpotService {
   }
 
   async update(id: string, data: UpdateParkingSpotDto) {
+    await this.isValidResource(id);
+
     return await this.prisma.spot.update({
       data,
       where: {
@@ -35,9 +72,54 @@ export class ParkingSpotService {
   }
 
   async remove(id: string) {
+    await this.isValidResource(id);
+
     return await this.prisma.spot.delete({
       where: {
         id,
+      },
+    });
+  }
+
+  private async isValidResource(id: string): Promise<void> {
+    const resourceToBeDeleted = await this.prisma.spot.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!resourceToBeDeleted) {
+      throw new NotFoundException('RESOURCE not found!', {
+        cause: new Error(),
+        description: 'The RESOURCE was not found in the Database',
+      });
+    }
+  }
+
+  private async existisByCarLicensePlate(carLicensePlate: string) {
+    return await this.prisma.spot.findFirst({
+      where: {
+        car_license_plate: carLicensePlate,
+      },
+    });
+  }
+
+  private async existsApartmentAndTowerConflict(
+    apartment: string,
+    tower: string,
+  ) {
+    return await this.prisma.spot.findFirst({
+      where: {
+        apartment,
+        tower,
+      },
+    });
+  }
+
+  private async existisBySpotNumber(spotNumber: string) {
+    return await this.prisma.spot.findFirst({
+      where: {
+        spot_number: spotNumber,
       },
     });
   }
